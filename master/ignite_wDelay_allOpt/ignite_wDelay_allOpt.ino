2a7b3677f9a006edc86b42ec4e65b5ebb5748e8a
Daisuke Shimaoka <daisuke.shimaoka@monash.edu>
2025-08-06 13:51:25 +1000

    revised to work only with busy signal
    status expos signal is not written
    must use polarity=0 and detectEdge=1
    tested to work at 30 and 60Hz

--- master/ignite_wDelay_allOpt/ignite_wDelay_allOpt.ino
+++ master/ignite_wDelay_allOpt/ignite_wDelay_allOpt.ino
@@ -8,11 +8,11 @@ const boolean polarity = 0; //1: spark = HIGH, 0:spark = LOW
 const boolean detectEdge = 1; //1:detect rising edge, 0:detect falling edge
 
 // camera parameter
-const unsigned int useStatusExpos = 1; //which pco output to use 1: statusExpos, 0: busy
-const unsigned int exposureTime = 4; //[ms] 10
+const unsigned int useStatusExpos = 0; //which pco output to use 1: statusExpos, 0: busy
+const unsigned int exposureTime = 4; //[ms] 4
 const unsigned int nRows = 900;
 const unsigned int lineTime = 12.136; //[us] fixed
-const unsigned int busyTime = 1000; //[us] guess!
+const unsigned int bufferTime = 100; //[us] delay to start laser after end of
 // timeline parameter
 const unsigned int frameRate = 60; //[Hz] 30
 
@@ -36,12 +36,12 @@ volatile unsigned int prescaler_duration;
 void setup()
 {
   if (useStatusExpos == 0) { //use busy output from camera
-    sparkDelayTime = 1e3*exposureTime + nRows*lineTime;   // microseconds. min ~100 max 1024000 (=1.024s)
-    sparkOnTime = ceil(1e6/frameRate) - 1e3*exposureTime - sparkDelayTime - busyTime;     // microseconds. max 1024000 (=1.024s)
+    sparkDelayTime = 1e3*exposureTime + nRows*lineTime +bufferTime;   // microseconds. min ~100 max 1024000 (=1.024s)
+    sparkOnTime = ceil(1e6/frameRate) - sparkDelayTime -bufferTime;     // microseconds. max 1024000 (=1.024s)
   }
   else { //use statusExpos output from camera
-    sparkDelayTime = isrDelayFactor;
-    sparkOnTime = ceil(1e6/frameRate) - 1e3*exposureTime - nRows*lineTime;     // microseconds. max 1024000 (=1.024s)
+   // sparkDelayTime = isrDelayFactor;
+   // sparkOnTime = ceil(1e6/frameRate) - 1e3*exposureTime - nRows*lineTime;     // microseconds. max 1024000 (=1.024s)
   }
 
   TCCR1A = 0;  // normal mode
